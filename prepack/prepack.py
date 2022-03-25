8888aafd6b9142587922c4e7ef52cd2aa68b92f6
Cargo <91343671+CargoCodes@users.noreply.github.com>
2022-03-25 16:17:39 +0100

    Update prepack.py

--- prepack/prepack.py
+++ prepack/prepack.py
@@ -1,12 +1,12 @@
-import click, os, subprocess
+import click, os
 
 @click.command()
-@click.option('--name', '-n', help='Name of the package to be prepared.', required=True)
+@click.argument('name', required=True)
 def prepack(name):
     os.system(f'mkdir {name}_package; cd {name}_package; mkdir {name}')
     with open(f'{name}_package/setup.py', 'w') as set:
-        set.write(f'from setuptools import setup\n\nsetup(\n    name=\'{name}\',\n    author=\'\',\n    version=\'1.0.0\',\n    packages=[\'{name}\'],\n    entry_points=\'\'\'\'\'\'\n)')
-        
+        set.write(f'from setuptools import setup\n\nsetup(\n    name=\'{name}\',\n    author=\'\',\n    version=\'0.0.0\',\n    packages=[\'{name}\'],\n    entry_points=\'\'\'\'\'\'\n)')
+
     with open(f'{name}_package/{name}/__init__.py', 'w') as ini:
         ini.write(f'from .{name} import *')
 
@@ -16,4 +16,14 @@ def prepack(name):
 @click.command()
 def build():
     os.system('pip install .')
-    os.system('python setup.py sdist bdist_wheel')
+    try:
+        os.system('python setup.py sdist bdist_wheel')
+    except:
+        os.system('python3 setup.py sdist bdist_wheel')
+
+@click.command()
+def pypi():
+    try:
+        os.system('python -m twine upload dist/*')
+    except:
+        os.system('python3 -m twine upload dist/*')
